97634e3092a7fa5115acfb5eb7992fad439bcc46
Jim Hughes <jim@lolno.net>
2025-02-16 22:57:36 -0600

    Add test reporting to CI/CD

--- vitest.config.base.ts
+++ vitest.config.base.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, type ViteUserConfig } from 'vitest/config';
 
-export default defineConfig({
-  test: {
-    include: ['src/**/*.unit.test.ts', '__tests__/**/*.test.ts'],
-  },
-});
+export const defineAtlasVitestConfig = (overrides: {
+  junitSuiteName?: string;
+}): ViteUserConfig =>
+  defineConfig({
+    test: {
+      include: ['src/**/*.unit.test.ts', '__tests__/**/*.test.ts'],
+      reporters: [
+        ['default'],
+        [
+          'junit',
+          {
+            suiteName: overrides.junitSuiteName ?? 'vitest tests',
+            outputFile: `./build/junit-${overrides.junitSuiteName?.replaceAll('/', '-') ?? 'tests'}.xml`,
+          },
+        ],
+      ],
+    },
+  });
